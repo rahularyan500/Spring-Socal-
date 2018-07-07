597dcce29e9fbb683acefc5d40903e8182da7516
rahularyann <rahul.aryan500@gmail.com>
2018-07-07 13:04:43 +0530

    added git into local repo

---
+++ src/main/java/com/socal/demo/SpringSocalApplication.java
@@ -0,0 +1,12 @@
+package com.socal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocalApplication.class, args);
+	}
+}
